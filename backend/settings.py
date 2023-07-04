1df971f5269904641b597c1befa59168c9495755
uzairhyd <uzairhyd89@gmail.com>
2023-07-04 23:04:02 +0530

    updated

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sl3d=#zzhl%5lk8i4j^h(ikxmmvbcpe440gb7fhu(olt788c^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
