f673b09ef45e16f9d3aa52ac46510f7e8624cc9f
uzairhyd <uzairhyd89@gmail.com>
2023-07-04 19:36:33 +0530

    The first commit

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,61 @@
+import React , { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
+import { listProducts } from '../actions/productActions'
+
+function HomeScreen({history}) {
+  const dispatch = useDispatch()
+  const productList = useSelector(state => state.productList)
+  const {error,loading, products, page, pages} = productList
+  
+  let keyword = history.location.search
+  useEffect(() => {
+    dispatch(listProducts(keyword))
+  
+  }, [dispatch, keyword])
+
+
+  return (
+    <div>
+      {!keyword && <ProductCarousel /> }
+      <h1>Latest Products</h1>
+      { loading ? <Loader />
+        : error ? <Message variant='danger'>{error}</Message>
+          :
+          <div>
+            <Row>
+            {products.map(product => (
+                <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                <Product product={product} />
+                </Col>
+            ))}
+          </Row>
+          <Paginate page={page} pages={pages} keyword={keyword} />
+        </div>
+    } 
+    </div>
+  )
+}
+
+export default HomeScreen
+
+
+//import axios from 'axios';
+// const [products, setProducts] = useState([])
+  
+// useEffect(() => {
+
+//   async function fetchProducts(){
+    
+//     const { data } = await axios.get('/api/products/')
+//     setProducts(data)
+//   }
+  
+//   fetchProducts();
+
+// }, [])
